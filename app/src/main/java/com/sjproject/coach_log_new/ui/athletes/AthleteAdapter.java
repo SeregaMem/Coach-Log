e030018ccc695622b29dd77f8ac10c4c5463bbe7
SeregaMem <seregamems@gmail.com>
2022-06-05 17:16:14 +0700

    BEFORE refactor AthleteFragment (no working). try to make List connection with DB

--- app/src/main/java/com/sjproject/coach_log_new/ui/athletes/AthleteAdapter.java
+++ app/src/main/java/com/sjproject/coach_log_new/ui/athletes/AthleteAdapter.java
@@ -1,6 +1,8 @@
 package com.sjproject.coach_log_new.ui.athletes;
 
 import android.content.Context;
+import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
 import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -10,22 +12,45 @@ import android.widget.TextView;
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
+import com.sjproject.coach_log_new.DBHelper;
 import com.sjproject.coach_log_new.R;
+
 
 public class AthleteAdapter extends RecyclerView.Adapter<AthleteAdapter.AthleteViewHolder> {
 
-    String LOG_TAG = "myLOG";
+    String LOG_TAG = "myLog";
+
+    int moveToFirstCount = 0;
+
     private int countItems;
 
-    public AthleteAdapter(int athletesCount) {
-        Log.d(LOG_TAG, "AthleteAdapter IN");
+    private Context parent;
+
+    DBHelper dbHelper;
+    SQLiteDatabase db;
+    Cursor c;
+
+    int nameColIndex = 1;
+    int bdayColIndex = 3;
+    String name;
+    String bday;
+
+    public AthleteAdapter(int athletesCount, Context parent) {
         countItems = athletesCount;
+
+        dbHelper = new DBHelper(parent);
+        db = dbHelper.getWritableDatabase();
+
+        c = db.query("athletesTable", null, null, null,
+                null, null, null);
+
+        c.moveToFirst();
+        this.parent = parent;
     }
 
     @NonNull
     @Override
     public AthleteViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
-        Log.d(LOG_TAG, "onCreateViewHolder IN");
         Context context = parent.getContext();
         int layoutIdForListItem = R.layout.item_athlete;
 
@@ -34,19 +59,35 @@ public class AthleteAdapter extends RecyclerView.Adapter<AthleteAdapter.AthleteV
         View view = inflater.inflate(layoutIdForListItem, parent, false);
 
         AthleteViewHolder viewHolder = new AthleteViewHolder(view);
+
+        if (moveToFirstCount == 0) {
+            dbHelper = new DBHelper(context);
+            db = dbHelper.getWritableDatabase();
+
+            c = db.query("athletesTable", null, null, null,
+                    null, null, null);
+
+            c.moveToFirst();
+        } else {
+            c.moveToNext();
+        }
+
+        name = c.getString(nameColIndex);
+        bday = c.getString(bdayColIndex);
+
+        moveToFirstCount++;
+
         return viewHolder;
     }
 
     @Override
     public void onBindViewHolder(@NonNull AthleteViewHolder holder, int position) {
-        Log.d(LOG_TAG, "onBindViewHolder IN");
-        // отрисовка
+        holder.bind();
     }
 
     @Override
     public int getItemCount() {
-        Log.d(LOG_TAG, "getItemCount IN");
-        return 100;
+        return countItems;
     }
 
     class AthleteViewHolder extends RecyclerView.ViewHolder {
@@ -55,11 +96,32 @@ public class AthleteAdapter extends RecyclerView.Adapter<AthleteAdapter.AthleteV
 
         public AthleteViewHolder(@NonNull View itemView) {
             super(itemView);
-
-            Log.d(LOG_TAG, "AthleteViewHolder IN");
-
             athleteNameView = (TextView) itemView.findViewById(R.id.tv_athleteName);
             athletebdayView = (TextView) itemView.findViewById(R.id.tv_athlete_bday);
+
+            itemView.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View v) {
+                    /* dbHelper = new DBHelper(parent);
+                    db = dbHelper.getWritableDatabase();
+
+                    Log.d(LOG_TAG, "delClick");
+                    String deleteName = athleteNameView.getText().toString();
+                    db.delete("athletesTable", "athlete_name = ?",
+                            new String[]{String.valueOf(deleteName)});
+
+                    db.close();
+                    dbHelper.close(); */
+                }
+            });
+        }
+
+        void bind() {
+            athleteNameView.setText(name);
+            athletebdayView.setText(bday);
+
+            db.close();
+            dbHelper.close();
         }
     }
 }
--- app/src/main/java/com/sjproject/coach_log_new/ui/athletes/AthletesFragment.java
+++ app/src/main/java/com/sjproject/coach_log_new/ui/athletes/AthletesFragment.java
@@ -68,41 +68,9 @@ public class AthletesFragment extends Fragment {
 
         rvAthletes.setHasFixedSize(true);
 
-        athleteAdapter = new AthleteAdapter(getAthletesCount());
+        athleteAdapter = new AthleteAdapter(getAthletesCount(), getActivity());
 
         rvAthletes.setAdapter(athleteAdapter);
-
-
-        /* Cursor c = db.query("athletesTable", null, null, null,
-                null, null, null);
-
-        if (c.moveToFirst()) {
-            int idCcolIndex = c.getColumnIndex("id");
-            int nameColIndex = c.getColumnIndex("athlete_name");
-            int phoneColIndex = c.getColumnIndex("phoneNumber");
-            int bdayColIndex = c.getColumnIndex("bday");
-
-            LinearLayout.LayoutParams lParams = new LinearLayout.LayoutParams(
-                    ActionBar.LayoutParams.MATCH_PARENT,
-                    ActionBar.LayoutParams.WRAP_CONTENT);
-
-            do {
-                Button btnNew = new Button(getActivity());
-                btnNew.setBackgroundColor(0xFFFFFF);
-                btnNew.setText(c.getString(nameColIndex) + "   т." + c.getString(phoneColIndex));
-                btnNew.setOnClickListener(new View.OnClickListener() {
-                    @Override
-                    public void onClick(View v) {
-                        startActivity(new Intent(getActivity(), AthleteDetails.class));
-                    }
-                });
-                rvAthletes.addView(btnNew);
-            } while (c.moveToNext());
-        }
-
-        c.close();
-
-        dbHelper.close(); */
     }
 
     public static int getAthletesCount () {
